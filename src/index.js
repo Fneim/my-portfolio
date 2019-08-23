cbd3374e96d4436563bdc93f0fa52b88ba726f65
fneim <neimfargin@gmx.com.tr>
2019-08-23 03:50:05 -0700

    single page site, color change

--- src/index.js
+++ src/index.js
@@ -3,27 +3,30 @@ $(document).ready(function() {
 
   $("#year").text(new Date().getFullYear());
 
-  $("#work-section").css("display", "none");
-  $("#contact-section").css("display", "none");
+  // $("#work-section").css("display", "none");
+  // $("#contact-section").css("display", "none");
 
-  $(".nav-link").on("click", function(e) {
-    e.preventDefault();
+  // $(".nav-link").on("click", function(e) {
+  //   e.preventDefault();
 
-    if ($(this).attr("id") === "about") {
-      console.log("about");
-      $("#about-section").css("display", "block");
-      $("#work-section").css("display", "none");
-      $("#contact-section").css("display", "none");
-    } else if ($(this).attr("id") === "work") {
-      console.log("work");
-      $("#about-section").css("display", "none");
-      $("#work-section").css("display", "block");
-      $("#contact-section").css("display", "none");
-    } else if ($(this).attr("id") === "contact") {
-      console.log("contact");
-      $("#about-section").css("display", "none");
-      $("#work-section").css("display", "none");
-      $("#contact-section").css("display", "block");
-    }
-  });
+  //   if ($(this).attr("id") === "about") {
+  //     console.log("about");
+  //     $("#about-section").css("display", "block");
+  //     $(".jumbotron").css("display", "block");
+  //     $("#work-section").css("display", "none");
+  //     $("#contact-section").css("display", "none");
+  //   } else if ($(this).attr("id") === "work") {
+  //     console.log("work");
+  //     $("#about-section").css("display", "none");
+  //     $(".jumbotron").css("display", "none");
+  //     $("#work-section").css("display", "block");
+  //     $("#contact-section").css("display", "none");
+  //   } else if ($(this).attr("id") === "contact") {
+  //     console.log("contact");
+  //     $("#about-section").css("display", "none");
+  //     $(".jumbotron").css("display", "none");
+  //     $("#work-section").css("display", "none");
+  //     $("#contact-section").css("display", "block");
+  //   }
+  // });
 });
